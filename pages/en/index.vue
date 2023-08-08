a176ddbc19ea8f50db8458f7259b72e1a3e4e0c5
Stepan Fedyanov <fedyanov@artlebedev.ru>
2023-08-08 12:45:58 +0300

    Change text and seo for en and ru versions

--- pages/en/index.vue
+++ pages/en/index.vue
@@ -16,9 +16,9 @@ export default {
       title: 'Fedyanov Stepan – Frontend Developer | CV',
       ogTitle: 'Fedyanov Stepan – Frontend Developer | CV',
       description:
-        'Frontend developer at VueJS. I am currently an intern at the Art. Lebedev Studio. I have been working as a freelancer for two years, participating in the development of large projects, as well as updating and refactoring existing solutions for clients. In my free time, I enjoy sports and reading.',
+        'Frontend developer in VueJS. Currently an intern at the Art. Lebedev Studio. I have developed over 20 projects, 15 of which received the highest rating from the client. I was awarded the special status "Rising Star" for my speed and quality of work.',
       ogDescription:
-        'Frontend developer at VueJS. I am currently an intern at the Art. Lebedev Studio. I have been working as a freelancer for two years, participating in the development of large projects, as well as updating and refactoring existing solutions for clients. In my free time, I enjoy sports and reading.',
+        'Frontend developer in VueJS. Currently an intern at the Art. Lebedev Studio. I have developed over 20 projects, 15 of which received the highest rating from the client. I was awarded the special status "Rising Star" for my speed and quality of work.',
       ogImage: '/ogImageEn.png',
       twitterCard: 'summary_large_image',
     });
